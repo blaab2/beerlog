b7a262ff2e8f3e0c451394c135b1bd540ea96720
Daniel Ossig <daniel@ossig-mail.de>
2020-06-09 16:17:50 +0200

    move dsgvo link to footer and update dsgvo adress

--- resources/views/layouts/_layout.blade.php
+++ resources/views/layouts/_layout.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
-	<script src="{{ mix('js/tools.js') }}"></script>
+    <script src="{{ mix('js/tools.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,88 +19,96 @@
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-	<link href="{{ mix('css/toastr.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/toastr.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-					<i class="fas fa-beer text-success"></i>
-				</a>
-				
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+                <i class="fas fa-beer text-success"></i>
+            </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                     <ul class="navbar-nav mr-auto">
-					 @auth
-    					<li class="nav-item">
-							<a class="nav-link" href="{{ route('users.index') }}">{{ __('List') }}</a>
-						</li>
-						@can('show details')<li class="nav-item">
-							<a class="nav-link" href="{{ route('settings.index') }}">{{ __('Settings') }}</a>
-						</li>
-						@endcan		
-					@endauth
-						<li class="nav-item">
-							<a class="nav-link" href="/impressum">{{ __('Datenschutz') }}</a>
-						</li>
-	                </ul>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('users.index') }}">{{ __('List') }}</a>
+                        </li>
+                        @can('show details')
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('settings.index') }}">{{ __('Settings') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->nickname }} <span class="caret"></span>
+                        @endcan
+                    @endauth
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->nickname }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
-        </nav>
-		@include('cookieConsent::index')
-		 @yield('body_content')
-    </div>
-	 @yield('welcome_content')
-	
-	@if(session()->has('flash_message'))
-		<script>toastr["success"]('{{session()->get('flash_message')}}');</script> 
-	@endif
-	@if(session()->has('flash_error'))
-		<script>toastr["error"]('{{session()->get('flash_error')}}');</script> 
-	@endif
-	
+        </div>
+    </nav>
+    @include('cookieConsent::index')
+    @yield('body_content')
+</div>
+@yield('welcome_content')
 
-	
-	
-	
+
+<footer class="footer fixed-bottom bg-dark">
+    <div class="container">
+        <span class="text-muted">Beerlog 2020 </span>
+        <a href="{{route('impressum')}}">{{ __('Datenschutz und Impressum') }}</a>
+    </div>
+</footer>
+
+
+@if(session()->has('flash_message'))
+    <script>toastr["success"]('{{session()->get('flash_message')}}');</script>
+@endif
+@if(session()->has('flash_error'))
+    <script>toastr["error"]('{{session()->get('flash_error')}}');</script>
+@endif
+
+
 </body>
 </html>
