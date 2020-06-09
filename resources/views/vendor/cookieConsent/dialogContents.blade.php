74222440c78c2b05b85b95f9ea3cf39ff0b8eb84
Daniel Ossig <daniel@ossig-mail.de>
2020-06-09 14:38:35 +0200

    update cookie  constent dialog

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,14 +1,14 @@
-<div class="js-cookie-consent cookie-consent">
-<div class="d-flex justify-content-center">
+<div class="fixed-top js-cookie-consent cookie-consent">
+    <div class="d-flex justify-content-center">
 
-<div class="alert alert-success" role="alert">
+        <div class="alert alert-success" role="alert">
     <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
 
-    <button class="btn btn-info js-cookie-consent-agree cookie-consent__agree">
-        {{ trans('cookieConsent::texts.agree') }}
-    </button>
-</div>
-</div>
+            <button class="btn btn-info js-cookie-consent-agree cookie-consent__agree">
+                {{ trans('cookieConsent::texts.agree') }}
+            </button>
+        </div>
+    </div>
 </div>
