08c062ebc840459772239c2b5bfcb0248bfea8c9
Daniel Ossig <daniel@ossig-mail.de>
2020-07-30 09:59:19 +0200

    change user index table dom

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -112,7 +112,9 @@
                                                 "visible": true
                                             }
                                         ],
-                                        dom: 'frtipB',
+                                        dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+                                            "<'row'<'col-sm-12'tr>>" +
+                                            "<'row'<'col-sm-12 col-md-5'B><'col-sm-12 col-md-7'p>>i",
                                         buttons: [
                                             'csv', 'excel'
                                         ],
